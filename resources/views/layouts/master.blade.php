25235c728ae02758bc1fe9a2ed4a7d8e276c1d43
ivaldyp <vivaaldy@gmail.com>
2020-09-03 11:21:11 +0700

    ubah hitstats

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -239,18 +239,18 @@
 					<div class="col-sm-6" >
 						<div class="footer-copyright pull-right">
 						  	<!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
-							<!-- Histats.com  START  (aync)-->
-							<script type="text/javascript">var _Hasync= _Hasync|| [];
-							_Hasync.push(['Histats.start', '1,3757099,4,603,110,40,00011000']);
-							_Hasync.push(['Histats.fasi', '1']);
-							_Hasync.push(['Histats.track_hits', '']);
-							(function() {
-							var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
-							hs.src = ('//s10.histats.com/js15_as.js');
-							(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
-							})();</script>
-							<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?3757099&101" alt="" border="0"></a></noscript>
-							<!-- Histats.com  END  -->
+<!-- Histats.com  START  (aync)-->
+<script type="text/javascript">var _Hasync= _Hasync|| [];
+_Hasync.push(['Histats.start', '1,3757099,4,202,118,45,00011000']);
+_Hasync.push(['Histats.fasi', '1']);
+_Hasync.push(['Histats.track_hits', '']);
+(function() {
+var hs = document.createElement('script'); hs.type = 'text/javascript'; hs.async = true;
+hs.src = ('//s10.histats.com/js15_as.js');
+(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+})();</script>
+<noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?3757099&101" alt="" border="0"></a></noscript>
+<!-- Histats.com  END  -->
 							<!-- <img src="{{ ('/portal/public/img/photo/plusjakartalogo2.png') }}" alt="" height="100"> -->
 						</div>
 					</div>
