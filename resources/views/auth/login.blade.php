a1519fd365b183541ad7e7eb151a4cecf349ba12
ivaldyp <vivaaldy@gmail.com>
2020-09-21 14:48:18 +0700

    ubah png jadi png32

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,7 @@
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
                         <div>
-                            <img src="public/img/photo/bpad-logo-04b.png" style="height: auto; max-height: 100%; max-width: 100%">
+                            <img src="public/img/photo/bpad-logo-04b.png32" style="height: auto; max-height: 100%; max-width: 100%">
                         </div>
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right"> Username </label>
