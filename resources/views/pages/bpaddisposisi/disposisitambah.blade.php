ab585e9090dd9615c8780b20ec5a75c61d6fc0cc
Ivaldy Putra <vivaaldy@gmail.com>
2021-09-22 22:16:30 +0700

    mematikan tombol disposisi tambah

--- resources/views/pages/bpaddisposisi/disposisitambah.blade.php
+++ resources/views/pages/bpaddisposisi/disposisitambah.blade.php
@@ -281,19 +281,17 @@
 								</div>
 							</div>
 							<div class="panel-footer">
-                                <!-- <button type="submit" class="btn btn-success pull-right">Simpan</button> -->
+                                {{-- <!-- <button type="submit" class="btn btn-success pull-right">Simpan</button> -->
                                 <input type="submit" name="btnKirim" class="btn btn-info pull-right m-r-10" value="Kirim">
                                 <input type="submit" name="btnDraft" class="btn btn-warning pull-right m-r-10" value="Draft">
-                                <!-- <button type="button" class="btn btn-default pull-right" style="margin-right: 10px" data-dismiss="modal">Kembali</button> -->
+                                <!-- <button type="button" class="btn btn-default pull-right" style="margin-right: 10px" data-dismiss="modal">Kembali</button> --> --}}
                                 <a href="/portal/disposisi/formdisposisi"><button type="button" class="btn btn-default pull-right m-r-10" onclick="goBack()">Kembali</button></a>
                                 <div class="clearfix"></div>
                             </div>
-						</div>	
-						<div class="panel panel-info">
 							<div class="panel-heading">  
 								
 							</div>
-						</div>
+						</div>	
 					</form>
 				</div>
 			</div>
