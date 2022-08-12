d22cca916d8a205b27dd6024d585e061e135c784
Valdy Putra <vivaaldy@gmail.com>
2022-08-12 09:19:52 +0700

    model baru, model password sapu jagad

---
+++ app/Models11/Password_sapu_jagad.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models11;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Password_sapu_jagad extends Model
+{
+    protected $connection = 'server11';
+    // protected $primaryKey = "id_emp"; 
+    protected $table = "asetmaster.app.password_sapu_jagad";
+    
+    public $incrementing = 'false';
+    public $timestamps = false;
+}
