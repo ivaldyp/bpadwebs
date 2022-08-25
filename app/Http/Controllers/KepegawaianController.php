0137fcf0296b103c280a5aae8c8caeb9d5b99861
Valdy Putra <vivaaldy@gmail.com>
2022-08-25 08:28:17 +0700

    ubah bidang di excel admin pegawai

--- app/Http/Controllers/KepegawaianController.php
+++ app/Http/Controllers/KepegawaianController.php
@@ -2811,7 +2811,7 @@ class KepegawaianController extends Controller
 			if(strlen($employee['kd_unit']) == 6) {
 				$bidangnow = $employee['nm_unit'];
 			} elseif (strlen($employee['kd_unit']) == 2) {
-				$bidangnow = "KEPALA BADAN PENGELOLAAN ASET DAERAH";
+				$bidangnow = "BADAN PENGELOLAAN ASET DAERAH";
 			}
 
 			$sheet->setCellValue($alphabet[$alpnum].$nowrow, $key+1); $alpnum++;
