1281fab1068f0fe1b4d8169126a615c97d9b97c0
Valdy Putra <vivaaldy@gmail.com>
2022-10-28 10:03:40 +0700

    tampilin pejabat foto

--- resources/views/pages/landingprofil/profilpejabat.blade.php
+++ resources/views/pages/landingprofil/profilpejabat.blade.php
@@ -58,6 +58,8 @@
                 @endif
                 <div class="col-md-3">
                     <div class="number">
+                        <?php elseif ( file_exists(config('app.savefileimgpejabat') . "\\" . $data['kd_unit'] . ".jpg") ) : ?>
+                            <img src="{{ config('app.openfileimgpejabat') }}{{  $data['kd_unit'] }}.jpg }}" style="width: 75%" alt="img">
                         <?php if (file_exists(config('app.savefileimg') . "\\" . $data['id_emp'] . "\\profil\\" . $data['foto'])) : ?>
                             <img src="{{ config('app.openfileimg') }}/{{ $data['id_emp'] }}/profil/{{ $data['foto'] }}" style="width: 75%" alt="img">
                         <?php else : ?>
