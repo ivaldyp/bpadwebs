d3bda905a79a99c70fd591ef31798bc20341d3d1
Valdy Putra <vivaaldy@gmail.com>
2022-11-16 12:28:41 +0700

    ubah model log login portal jadi bisa local bisa di server

--- app/Models76/Log_bpad.php
+++ app/Models76/Log_bpad.php
@@ -6,7 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Log_bpad extends Model
 {
-    protected $connection = 'server76';
+    protected $connection = 'sqlsrv2';
 	protected $table = "bpaddtfake.dbo.SYSLOG_BPADPORTAL";
-	public $timestamps = false;
+    public $incrementing = 'false';
+    public $timestamps = false;
 }
