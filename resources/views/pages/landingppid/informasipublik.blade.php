8f6c728a879a57ec55c4f387a1f84f65a9b2f9a7
Valdy Putra <vivaaldy@gmail.com>
2023-03-28 10:34:33 +0700

    tambah program&kegiatan dan laporan akuntabilitas kinerja di halaman ppid

--- resources/views/pages/landingppid/informasipublik.blade.php
+++ resources/views/pages/landingppid/informasipublik.blade.php
@@ -22,36 +22,55 @@
 					<!-- article content -->
 					<div class="article-content">
 			            
+                        <!-- 1 -->
                         <button class="accordion"><span style="font-weight: bold;">Profil PPID</span></button>
                         <div class="panel">
                             <p><a target="_blank" href="https://bpad.jakarta.go.id/portal/ppid/profil">Halaman Profil PPID</a></p>
                         </div>
 
+                        <!-- 2 -->
                         <button class="accordion"><span style="font-weight: bold;">Visi Misi</span></button>
                         <div class="panel">
                             <p><a target="_blank" href="https://bpad.jakarta.go.id/portal/profil/visimisi">Halaman Visi & Misi BPAD</a></p>
                         </div>
 
+                        <!-- 3 -->
                         <button class="accordion"><span style="font-weight: bold;">Tugas & Fungsi</span></button>
                         <div class="panel">
                             <p><a target="_blank" href="https://bpad.jakarta.go.id/portal/profil/tupoksi">Halaman Tugas & Fungsi BPAD</a></p>
                         </div>
 
+                        <!-- 4 -->
                         <button class="accordion"><span style="font-weight: bold;">Struktur Organisasi</span></button>
                         <div class="panel">
                             <p><a target="_blank" href="https://bpad.jakarta.go.id/portal/profil/struktur">Halaman Struktur Organisasi BPAD</a></p>
                         </div>
                         
+                        <!-- 5 -->
                         <button class="accordion"><span style="font-weight: bold;">Profil Pejabat</span></button>
                         <div class="panel">
                             <p><a target="_blank" href="https://bpad.jakarta.go.id/portal/profil/profilpejabat">Halaman Profil Pejabat BPAD</a></p>
                         </div>
 
+                        <!-- 6 -->
                         <button class="accordion"><span style="font-weight: bold;">Daftar Pegawai BPAD</span></button>
                         <div class="panel">
                             <p><a target="_blank" href="https://drive.google.com/file/d/1LG7lHx2BER_3-L8kknXBTFczkF_tPrGN/view?usp=share_link">Unduh Daftar Pegawai BPAD</a></p>
                         </div>
 
+                        <!-- 7 -->
+                        <button class="accordion"><span style="font-weight: bold;">Program dan Kegiatan BPAD</span></button>
+                        <div class="panel">
+                            <p><a target="_blank" href="https://publik.bapedadki.net/ringkasan_skpd/981/home">Lihat Program dan Kegiatan BPAD</a></p>
+                        </div>
+
+                        <!-- 9 -->
+                        <button class="accordion"><span style="font-weight: bold;">Laporan Akuntabilitas Kinerja</span></button>
+                        <div class="panel">
+                            <p><a target="_blank" href="https://ppid.jakarta.go.id/laporan-kinerja-instansi-pemerintah">Lihat Laporan Akuntabilitas Kinerja</a></p>
+                        </div>
+
+                        <!-- 10 -->
                         <button class="accordion"><span style="font-weight: bold;">Laporan Keuangan BPAD</span></button>
                         <div class="panel">
                             <ul>
@@ -59,11 +78,13 @@
                             </ul>
                         </div>
 
+                        <!-- 11 -->
                         <button class="accordion"><span style="font-weight: bold;">Rencana Kerja dan Anggaran</span></button>
                         <div class="panel">
                             <p><a target="_blank" href="https://publik.bapedadki.net/ringkasan_skpd/981/home">Tahun Anggaran 2023</a></p>
                         </div>
                         
+                        <!-- 32 -->
                         <button class="accordion"><span style="font-weight: bold;">Surat Keputusan Penetapan PPID BPAD</span></button>
                         <div class="panel">
                             <ul>
