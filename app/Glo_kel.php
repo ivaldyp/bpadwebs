c92763ce87f36293250d5fa3f0e351067c3bbb0d
Valdy Putra <vivaaldy@gmail.com>
2024-06-03 09:01:25 +0700

    fix penulisan tabel pada model

--- app/Glo_kel.php
+++ app/Glo_kel.php
@@ -8,7 +8,7 @@ class Glo_kel extends Model
 {
     protected $connection = 'sqlsrv2';
     // protected $primaryKey = "id_emp"; 
-    protected $table = "glo_kel";
+    protected $table = "bpaddtfake.dbo.glo_kel";
     
     public $incrementing = 'false';
     public $timestamps = false;
